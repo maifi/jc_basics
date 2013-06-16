fb65070c5d89fd05869f61717db4232d4a7e58b1
Christian Maierhofer <christian.maierhofer@student.tugraz.at>
2013-06-16 21:18:41 +0200

    rsa now using 1024 bit keys

--- src/jcaes/JcAES.java
+++ src/jcaes/JcAES.java
@@ -5,11 +5,6 @@ import javacard.framework.Applet;
 import javacard.framework.ISO7816;
 import javacard.framework.ISOException;
 import javacard.framework.Util;
-import javacard.security.AESKey;
-import javacard.security.CryptoException;
-import javacard.security.DESKey;
-import javacard.security.Key;
-import javacard.security.KeyBuilder;
 import javacard.security.KeyPair;
 import javacard.security.RSAPrivateKey;
 import javacard.security.RSAPublicKey;
@@ -57,12 +52,14 @@ public class JcAES extends Applet {
 	byte[] certificate;
 	short cert_len;
 	short cert_block_size = 120;
+	
 	RSAPrivateKey _rsaPrivKey = null;
 	RSAPublicKey _rsaPubKey = null;
 
 	byte[] reply = null;
 
-
+	short rsa_block_size = 128;
+	short key_size_bits = 1024;
 
 	public static void install(byte[] buffer, short offset, byte length) {
 		new JcAES();
@@ -70,12 +67,16 @@ public class JcAES extends Applet {
 
 	private JcAES(){
 		//allocate all memory
-		dataToEncrypt = new byte[64];
-		dataToDecrypt = new byte[64];
-		result = new byte[64];
+		dataToEncrypt = new byte[rsa_block_size];
+		dataToDecrypt = new byte[rsa_block_size];
+		
+		rsaEncrypted = new byte[rsa_block_size];
+		rsaDecrypted = new byte[rsa_block_size];
+		
+		result = new byte[rsa_block_size];
 
 		//KeyAgreement dhKeyAgreement = KeyAgreement.getInstance(KeyAgreement.ALG_EC_SVDP_DH, false);
-		_rsaKeyPair = new KeyPair(KeyPair.ALG_RSA, (short) 512);
+		_rsaKeyPair = new KeyPair(KeyPair.ALG_RSA, (short) key_size_bits);
 		_rsaKeyPair.genKeyPair();
 
 		_rsaPrivKey = (RSAPrivateKey) _rsaKeyPair.getPrivate();
@@ -85,8 +86,7 @@ public class JcAES extends Applet {
 		//_signature = Signature.getInstance(Signature.ALG_RSA_SHA_PKCS1, false);
 
 		//rsaCipher = Cipher.getInstance(Cipher.ALG_RSA_PKCS1, false);
-		rsaEncrypted = new byte[64];
-		rsaDecrypted = new byte[64];
+		
 		//exponent = new byte[16];
 		//modulus = new byte[64];
 
@@ -94,9 +94,6 @@ public class JcAES extends Applet {
 		cert_len = 0;
 
 		reply = new byte[256];
-
-
-
 
 		register();
 	}
@@ -114,66 +111,66 @@ public class JcAES extends Applet {
 
 		if (cmd[ISO7816.OFFSET_CLA] == CLASS) {  
 			switch(cmd[ISO7816.OFFSET_INS]) {      
-			case ENCRYPT:
-				//we dont care of P1 and P2 now
-				short data_len = (short)(cmd[ISO7816.OFFSET_LC] & 0x00FF);//must be 16
-
-				Util.arrayCopy(cmd, ISO7816.OFFSET_CDATA, dataToEncrypt, (short) 0, data_len);
-
-				result = aesEncrypt(dataToEncrypt);
-
-				apdu.setOutgoing();            
-				apdu.setOutgoingLength((short)result.length);
-				apdu.sendBytesLong(result, (short)0, (short)result.length);
-				break;
-
-			case DECRYPT:
-				data_len = (short)(cmd[ISO7816.OFFSET_LC] & 0x00FF);
-				byte[] decryptedData;
-				Util.arrayCopy(cmd, ISO7816.OFFSET_CDATA, dataToDecrypt, (short) 0, data_len);
-
-				decryptedData = aesDecrypt(dataToDecrypt);
-
-				apdu.setOutgoing();            
-				apdu.setOutgoingLength((short)decryptedData.length);
-				apdu.sendBytesLong(decryptedData, (short)0, (short)decryptedData.length);
-				break;
-
-			case ENCRYPT_DES:
-				//we dont care of P1 and P2 now
-				data_len = (short)(cmd[ISO7816.OFFSET_LC] & 0x00FF);
-				Util.arrayCopy(cmd, ISO7816.OFFSET_CDATA, dataToEncrypt, (short) 0, data_len);
-
-				result = desEncrypt(dataToEncrypt);
-
-				apdu.setOutgoing();            
-				apdu.setOutgoingLength((short)result.length);
-				apdu.sendBytesLong(result, (short)0, (short)result.length);
-				break;
-
-			case DECRYPT_DES:
-				data_len = (short)(cmd[ISO7816.OFFSET_LC] & 0x00FF);
-				Util.arrayCopy(cmd, ISO7816.OFFSET_CDATA, dataToDecrypt, (short) 0, data_len);
-
-				decryptedData = desDecrypt(dataToDecrypt);
-
-				apdu.setOutgoing();            
-				apdu.setOutgoingLength((short)decryptedData.length);
-				apdu.sendBytesLong(decryptedData, (short)0, (short)decryptedData.length);
-				break;
-				//SIGN data
-			case SIGN_DATA:
-				_signature.init(_rsaPrivKey, Signature.MODE_SIGN);
-				byte[] test = {1,2,3,4,5};
-				byte[] test_signature = new byte[64];//generate 64 byte signature
-				_signature.sign(test, (short)0	, (short)test.length, test_signature, (short)0);
-
-				apdu.setOutgoing();            
-				apdu.setOutgoingLength((short)test_signature.length);
-				apdu.sendBytesLong(test_signature, (short)0, (short)test_signature.length);
-				break;
+//			case ENCRYPT:
+//				//we dont care of P1 and P2 now
+//				short data_len = (short)(cmd[ISO7816.OFFSET_LC] & 0x00FF);//must be 16
+//
+//				Util.arrayCopy(cmd, ISO7816.OFFSET_CDATA, dataToEncrypt, (short) 0, data_len);
+//
+//				result = aesEncrypt(dataToEncrypt);
+//
+//				apdu.setOutgoing();            
+//				apdu.setOutgoingLength((short)result.length);
+//				apdu.sendBytesLong(result, (short)0, (short)result.length);
+//				break;
+//
+//			case DECRYPT:
+//				data_len = (short)(cmd[ISO7816.OFFSET_LC] & 0x00FF);
+//				byte[] decryptedData;
+//				Util.arrayCopy(cmd, ISO7816.OFFSET_CDATA, dataToDecrypt, (short) 0, data_len);
+//
+//				decryptedData = aesDecrypt(dataToDecrypt);
+//
+//				apdu.setOutgoing();            
+//				apdu.setOutgoingLength((short)decryptedData.length);
+//				apdu.sendBytesLong(decryptedData, (short)0, (short)decryptedData.length);
+//				break;
+//
+//			case ENCRYPT_DES:
+//				//we dont care of P1 and P2 now
+//				data_len = (short)(cmd[ISO7816.OFFSET_LC] & 0x00FF);
+//				Util.arrayCopy(cmd, ISO7816.OFFSET_CDATA, dataToEncrypt, (short) 0, data_len);
+//
+//				result = desEncrypt(dataToEncrypt);
+//
+//				apdu.setOutgoing();            
+//				apdu.setOutgoingLength((short)result.length);
+//				apdu.sendBytesLong(result, (short)0, (short)result.length);
+//				break;
+//
+//			case DECRYPT_DES:
+//				data_len = (short)(cmd[ISO7816.OFFSET_LC] & 0x00FF);
+//				Util.arrayCopy(cmd, ISO7816.OFFSET_CDATA, dataToDecrypt, (short) 0, data_len);
+//
+//				decryptedData = desDecrypt(dataToDecrypt);
+//
+//				apdu.setOutgoing();            
+//				apdu.setOutgoingLength((short)decryptedData.length);
+//				apdu.sendBytesLong(decryptedData, (short)0, (short)decryptedData.length);
+//				break;
+//				//SIGN data
+//			case SIGN_DATA:
+//				_signature.init(_rsaPrivKey, Signature.MODE_SIGN);
+//				byte[] test = {1,2,3,4,5};
+//				byte[] test_signature = new byte[64];//generate 64 byte signature
+//				_signature.sign(test, (short)0	, (short)test.length, test_signature, (short)0);
+//
+//				apdu.setOutgoing();            
+//				apdu.setOutgoingLength((short)test_signature.length);
+//				apdu.sendBytesLong(test_signature, (short)0, (short)test_signature.length);
+//				break;
 			case RSA_ENCRYPT:
-				data_len = (short)(cmd[ISO7816.OFFSET_LC] & 0x00FF);
+				short data_len = (short)(cmd[ISO7816.OFFSET_LC] & 0x00FF);
 				rsaCipher = Cipher.getInstance(Cipher.ALG_RSA_PKCS1, false);
 				rsaCipher.init(_rsaPubKey, Cipher.MODE_ENCRYPT);
 				Util.arrayCopy(cmd, ISO7816.OFFSET_CDATA, dataToEncrypt, (short) 0, data_len);
@@ -187,9 +184,12 @@ public class JcAES extends Applet {
 			case RSA_DECRYPT:
 				data_len = (short)(cmd[ISO7816.OFFSET_LC] & 0x00FF);
 				rsaCipher = Cipher.getInstance(Cipher.ALG_RSA_PKCS1, false);
+				
 				rsaCipher.init(_rsaPrivKey, Cipher.MODE_DECRYPT);
+				
 				Util.arrayCopy(cmd, ISO7816.OFFSET_CDATA, dataToDecrypt, (short) 0, data_len);
-				number_bytes = rsaCipher.doFinal(dataToDecrypt, (byte)0, (short)64, rsaDecrypted, (byte)0);
+				
+				number_bytes = rsaCipher.doFinal(dataToDecrypt, (byte)0, (short)rsa_block_size, rsaDecrypted, (byte)0);
 
 				apdu.setOutgoing();
 				apdu.setOutgoingLength(number_bytes);
@@ -201,8 +201,8 @@ public class JcAES extends Applet {
 					_rsaPrivKey.getExponent(cmd, (short) 0);
 
 					apdu.setOutgoing();
-					apdu.setOutgoingLength((short)64);
-					apdu.sendBytesLong(cmd, (short)0, (short)64);
+					apdu.setOutgoingLength((short)rsa_block_size);
+					apdu.sendBytesLong(cmd, (short)0, (short)rsa_block_size);
 				}catch(Exception e){
 					ISOException.throwIt(ISO7816.SW_DATA_INVALID);
 				}
@@ -212,10 +212,9 @@ public class JcAES extends Applet {
 				try{
 					_rsaPrivKey.getModulus(reply, (short) 0);
 
-
 					apdu.setOutgoing();
-					apdu.setOutgoingLength((short)64);
-					apdu.sendBytesLong(reply, (short)0, (short)64);
+					apdu.setOutgoingLength((short)rsa_block_size);
+					apdu.sendBytesLong(reply, (short)0, (short)rsa_block_size);
 				}catch(Exception e){
 					ISOException.throwIt(ISO7816.SW_DATA_INVALID);
 				}
@@ -311,76 +310,76 @@ public class JcAES extends Applet {
 
 	} 
 
-	private byte[] aesEncrypt(byte[] data){
-
-		try{
-			Cipher cipher = Cipher.getInstance(Cipher.ALG_AES_BLOCK_128_CBC_NOPAD,false);
-			AESKey aeskey = (AESKey) KeyBuilder.buildKey(KeyBuilder.TYPE_AES, KeyBuilder.LENGTH_AES_128, false);
-			aeskey.setKey(_aesKey, (short) 0);
-
-			cipher.init((Key)aeskey, Cipher.MODE_ENCRYPT);// error here!
-
-			cipher.doFinal(data, (short) 0, (byte) data.length, result, (short) 0);
-			return result;
-		}catch(CryptoException c){
-
-		}catch(Exception e){
-
-		}
-		return null;
-	}
-
-	private byte[] aesDecrypt(byte[] data){
-
-		try{
-			Cipher cipher = Cipher.getInstance(Cipher.ALG_AES_BLOCK_128_CBC_NOPAD,false);
-			AESKey aeskey = (AESKey) KeyBuilder.buildKey(KeyBuilder.TYPE_AES, KeyBuilder.LENGTH_AES_128, false);
-			aeskey.setKey(_aesKey, (short) 0);
-			cipher.getAlgorithm();
-			cipher.init((Key)aeskey, Cipher.MODE_DECRYPT);
-
-			cipher.doFinal(data, (short) 0, (byte) result.length, result, (short) 0);
-			return result;
-		}catch(Exception e){
-
-		}
-		return null;
-	}
-
-	private byte[] desEncrypt(byte[] data){
-
-		try{
-			Cipher cipher = Cipher.getInstance(Cipher.ALG_DES_CBC_NOPAD,false);
-			DESKey deskey = (DESKey) KeyBuilder.buildKey(KeyBuilder.TYPE_DES, KeyBuilder.LENGTH_DES, false);
-			deskey.setKey(_desKey, (short) 0);
-
-			cipher.init((Key)deskey, Cipher.MODE_ENCRYPT);
-
-			cipher.doFinal(data, (short) 0, (byte) data.length, result, (short) 0);
-			return result;
-		}catch(CryptoException c){
-
-		}catch(Exception e){
-
-		}
-		return null;
-	}
-
-	private byte[] desDecrypt(byte[] data){
-
-		try{
-			Cipher cipher = Cipher.getInstance(Cipher.ALG_DES_CBC_NOPAD,false);
-			DESKey deskey = (DESKey) KeyBuilder.buildKey(KeyBuilder.TYPE_DES, KeyBuilder.LENGTH_DES, false);
-			deskey.setKey(_desKey, (short) 0);
-
-			cipher.init((Key)deskey, Cipher.MODE_DECRYPT);
-			//cipher.update(data, (short) 0, (byte) 0x08, result, (short) 0);
-			cipher.doFinal(data, (short) 0, (byte) 0x08, result, (short) 0);
-			return result;
-		}catch(Exception e){
-
-		}
-		return null;
-	}
+//	private byte[] aesEncrypt(byte[] data){
+//
+//		try{
+//			Cipher cipher = Cipher.getInstance(Cipher.ALG_AES_BLOCK_128_CBC_NOPAD,false);
+//			AESKey aeskey = (AESKey) KeyBuilder.buildKey(KeyBuilder.TYPE_AES, KeyBuilder.LENGTH_AES_128, false);
+//			aeskey.setKey(_aesKey, (short) 0);
+//
+//			cipher.init((Key)aeskey, Cipher.MODE_ENCRYPT);// error here!
+//
+//			cipher.doFinal(data, (short) 0, (byte) data.length, result, (short) 0);
+//			return result;
+//		}catch(CryptoException c){
+//
+//		}catch(Exception e){
+//
+//		}
+//		return null;
+//	}
+//
+//	private byte[] aesDecrypt(byte[] data){
+//
+//		try{
+//			Cipher cipher = Cipher.getInstance(Cipher.ALG_AES_BLOCK_128_CBC_NOPAD,false);
+//			AESKey aeskey = (AESKey) KeyBuilder.buildKey(KeyBuilder.TYPE_AES, KeyBuilder.LENGTH_AES_128, false);
+//			aeskey.setKey(_aesKey, (short) 0);
+//			cipher.getAlgorithm();
+//			cipher.init((Key)aeskey, Cipher.MODE_DECRYPT);
+//
+//			cipher.doFinal(data, (short) 0, (byte) result.length, result, (short) 0);
+//			return result;
+//		}catch(Exception e){
+//
+//		}
+//		return null;
+//	}
+//
+//	private byte[] desEncrypt(byte[] data){
+//
+//		try{
+//			Cipher cipher = Cipher.getInstance(Cipher.ALG_DES_CBC_NOPAD,false);
+//			DESKey deskey = (DESKey) KeyBuilder.buildKey(KeyBuilder.TYPE_DES, KeyBuilder.LENGTH_DES, false);
+//			deskey.setKey(_desKey, (short) 0);
+//
+//			cipher.init((Key)deskey, Cipher.MODE_ENCRYPT);
+//
+//			cipher.doFinal(data, (short) 0, (byte) data.length, result, (short) 0);
+//			return result;
+//		}catch(CryptoException c){
+//
+//		}catch(Exception e){
+//
+//		}
+//		return null;
+//	}
+//
+//	private byte[] desDecrypt(byte[] data){
+//
+//		try{
+//			Cipher cipher = Cipher.getInstance(Cipher.ALG_DES_CBC_NOPAD,false);
+//			DESKey deskey = (DESKey) KeyBuilder.buildKey(KeyBuilder.TYPE_DES, KeyBuilder.LENGTH_DES, false);
+//			deskey.setKey(_desKey, (short) 0);
+//
+//			cipher.init((Key)deskey, Cipher.MODE_DECRYPT);
+//			//cipher.update(data, (short) 0, (byte) 0x08, result, (short) 0);
+//			cipher.doFinal(data, (short) 0, (byte) 0x08, result, (short) 0);
+//			return result;
+//		}catch(Exception e){
+//
+//		}
+//		return null;
+//	}
 
 }
